bb47d62b6422b76bad0cc4b27f93a7f03292df96
VenkataJaswanth <85697901+VenkataJaswanth@users.noreply.github.com>
2022-04-04 12:52:02 +0530

    Add files via upload

---
+++ src/main/java/Store.java
@@ -0,0 +1,33 @@
+import entity.Catalog;
+import entity.Category;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Store {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+        Transaction t = session.beginTransaction();
+        Category category = new Category();
+        category.setCategoryName("Gift Card");
+        Catalog catalog = new Catalog();
+        catalog.setCatalog_Name("Amazon Gift Card");
+        catalog.setStatus("Active");
+        Catalog catalog1 = new Catalog();
+        catalog1.setCatalog_Name("Flipkart Gift Card");
+        catalog1.setStatus("Inactive");
+        category.getCatalogList().add(catalog);
+        category.getCatalogList().add(catalog1);
+        catalog.setCategory(category);
+        catalog1.setCategory(category);
+        session.persist(category);
+        t.commit();
+        System.out.println("Successfully Done");
+        session.close();
+        factory.close();
+    }
+}
